714fc750260eeb8e3d8ce4b5025a4584e9567b50
MarlyssonOliveira <marlysson.oliveira@hotmail.com>
2022-11-08 19:46:10 -0300

    commit inicial

---
+++ src/main/java/br/com/ifpe/pkafkaconsumer/Pratica08Application.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.pkafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratica08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratica08Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/pkafkaconsumer/Pratica08ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.pkafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
